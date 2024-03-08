87321dbe1d51d4a9c7471f78ecff075c1e141805
M NIETO <mrconietoprez@gmail.com>
2024-03-08 17:19:32 +0100

    added old web backup

---
+++ Viejaweb/photogallery/photo23141/_vti_cnf/sldshow.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|29 Nov 2009 10:04:15 -0000
+vti_author:SR|VALENCIA\\Pili
+vti_modifiedby:SR|VALENCIA\\Pili
+vti_nexttolasttimemodified:TW|29 Nov 2009 10:04:15 -0000
+vti_timecreated:TR|29 Nov 2009 09:55:15 -0000
+vti_lineageid:SR|{5853B051-407C-4D61-BCB6-43405C709596}
+vti_extenderversion:SR|5.0.2.2623
+vti_backlinkinfo:VX|photogallery/photo23141/real.htm
+vti_cacheddtm:TX|29 Nov 2009 09:55:15 -0000
+vti_filesize:IR|4362
